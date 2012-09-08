3518f60e708570315d8a8c4d5c256bf93d292137
Dmitry Lazurkin <dilaz03@gmail.com>
2012-09-08 10:58:25 +0300

    рефакторинг имен файлов, изменение отображения ссылок, исправление ссылок на этапы алгоритма из libscprg

--- clean.bat
+++ clean.bat
@@ -4,10 +4,13 @@ del /F /Q "auto"
 rmdir /Q /S "auto"
 
 del /F /Q _region_.*
+rmdir /S /Q _region_.*
+
 del /F /Q *.aux
 del /F /Q *.log
 del /F /Q *.out
 del /F /Q *.pdf
 del /F /Q *.rel
+del /F /Q *.toc
 
 del /F /Q *.bak
