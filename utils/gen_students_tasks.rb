5836b10794c9b621ec24ff3d6e00d9fbfc16422a
Dmitry Lazurkin <dilaz03@gmail.com>
2012-09-16 17:39:27 +0300

    скрипт для генерации тем расчетной работы для студентов

--- utils/gen_students_tasks.rb
+++ utils/gen_students_tasks.rb
@@ -1,41 +1,111 @@
+# encoding: utf-8
 
 $:.unshift File.dirname(__FILE__)
 
 require 'tasks_db'
 
 header = <<EOF
-\documentclass[a4paper,11pt]{report}
+\\documentclass[a4paper,11pt]{report}
 
-\usepackage[cm]{fullpage}
+\\usepackage[cm]{fullpage}
 
-\usepackage[english,russian]{babel}
-\usepackage[utf8]{inputenc}
-\usepackage[T2A]{fontenc}
-\usepackage{indentfirst}
-\usepackage{cmap}
+\\usepackage[english,russian]{babel}
+\\usepackage[utf8]{inputenc}
+\\usepackage[T2A]{fontenc}
+\\usepackage{indentfirst}
+\\usepackage{cmap}
+\\usepackage{tabularx}
+\\usepackage{array}
 
-\begin{document}
+\\begin{document}
 EOF
 
 footer = <<EOF
-\end{document}
+\\end{document}
 EOF
 
-curs = ['Основы искусственного интеллекта', 'Проектирование программ в интеллектуальных системах']
+curs = ['Основы искусственного интеллекта', 'Проетирование программ в интеллектуальных системах']
+
 paths = [
-  '\\Info\StudInfo\~Методическое обеспечение кафедры\~Учебные курсы\1 курс\ОИИ\Расчётная работа',
-  '\\Info\StudInfo\~Методическое обеспечение кафедры\~Учебные курсы\2 курс\ППвИС\1sem\Расчётная работа'
+  '\\\\Info\\StudInfo\\~Методическое обеспечение кафедры\\~Учебные курсы\\1 курс\\ОИИ\\Расчётная работа',
+  '\\\\Info\\StudInfo\\~Методическое обеспечение кафедры\\~Учебные курсы\\2 курс\\ППвИС\\1sem\\Расчётная работа'
 ]
 
+def end_group(to, last_group)
+  to.puts '  \hline'
+  to.puts '  \end{tabularx}'
+  to.puts '\end{table}'
+  to.puts
+  to.puts '\clearpage' unless last_group
+end
+
 if $PROGRAM_NAME == __FILE__
+  if ARGV.size != 1
+    puts "Error: Pass zero-based semester number."
+    exit(1)
+  end
+
+  sem = ARGV[0].to_i
   db = TasksDb.new 'tasks.csv'
 
   File.open 'students_tasks.tex', 'w' do |to|
     to.puts header
 
-    File.open 'students_tasks.in' do |from|
+    first_group = true
+    counter = 0
 
+    CSV.foreach 'students_tasks.csv', col_sep: ';' do |row|
+      if row[1]
+        counter = counter + 1
+
+        tgr_num = row[1].to_i - 1
+        t_num = row[2].to_i - 1
+
+        to << "  #{counter} & #{row[0]} &"
+
+        to << '{\scriptsize '
+        to << "#{db.groups[tgr_num][0]}: " if tgr_num == 0 or tgr_num == 1
+        to << "#{db.groups[tgr_num][1][t_num].name}"
+        to << ' (' << row[3..row.size - 1].join(', ') << ')' if row[3]
+        to << '}'
+
+        to.puts ' & ~ & ~ \\\\ \hline'
+        to.puts
+      else
+        if first_group
+          first_group = false
+        else
+          end_group to, false
+        end
+
+        to.puts '\begin{center}'
+        to << '  ' << "\\textbf{Группа #{row[0]}}" << "\n"
+        to.puts '\end{center}'
+
+        to.puts
+        to.puts "Материалы для выполния расчетной работы можно найти по следующему сетевому пути:"
+        to.puts '{\scriptsize'
+        to.puts '\begin{verbatim}'
+        to << paths[sem] << "\n"
+        to.puts '\end{verbatim}'
+        to.puts '}'
+
+        to.puts
+
+        to.puts "Задание расчетной работы по курсу <<#{curs[sem]}>> получил (-a):"
+
+        to.puts
+
+        to.puts '\begin{table}[ht]'
+        to.puts '  \centering'
+        to.puts '  \begin{tabularx}{\linewidth}{| c | c | X | c | c |}'
+        to.puts '  \hline'
+        to.puts '  № & ФИО студента & Задание по расчетной работе & Дата получения & Подпись \\\\ \hline'
+        counter = 0
+      end
     end
+
+    end_group to, true
 
     to.puts footer
   end
